dea9f03e973fdd2ab723d0e01f9d331b796d3f2b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-15 11:50:07 +0200

    Wrap over-length decode calls to the 100-column limit

--- Sources/Binary LEB128 Parser Primitives/Binary.LEB128.Signed.swift
+++ Sources/Binary LEB128 Parser Primitives/Binary.LEB128.Signed.swift
@@ -63,7 +63,9 @@ extension Binary.LEB128.Signed: Parser.`Protocol` {
             input.removeFirst()
             // Delegate to the shared decode core (Binary LEB128 Decode Primitives);
             // the signed step self-contains two's-complement sign extension.
-            if try Binary.LEB128.Decode.signed(byte: byte.underlying, into: &result, shift: &shift) {
+            if try Binary.LEB128.Decode.signed(
+                byte: byte.underlying, into: &result, shift: &shift
+            ) {
                 return result
             }
         }
